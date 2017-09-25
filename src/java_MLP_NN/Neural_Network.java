851e2667e7520764564d96f5b5850fd56a6e3f46
Sarah McDougald <sarahmcdougald2020@u.northwestern.edu>
2017-09-25 00:39:21 -0500

    beginning 3rd neural network version, in Java but with minimalistic structure

---
+++ src/java_MLP_NN/Neural_Network.java
@@ -0,0 +1,76 @@
+package java_MLP_NN;
+
+
+/**
+ * Created by Sarah on 9/25/2017.
+ *
+ * GOALS with this version of the project:
+ * -- flesh out architecture with pseudocode; THEN fill in with code, once complete
+ * -- Use the simplest NN design possible. Save Java's object focus for a later version.
+ * -- Get a neural network up and running, in Java.
+ */
+public class Neural_Network
+{
+    //Write it out in pseudocode...
+
+    /*
+
+
+
+     */
+
+
+    /**
+     * Constructor for Neural Network.
+     *
+     * Simplistic. Takes 3 variables: number of input nodes, number of hidden nodes, number of output nodes.
+     */
+    /*
+    public Neural_Network(int numberInputs, int numberHidden, int numberOutput)
+    {
+    // set up all variables. Random values set for weights. Compare to neural network attempt #2.
+
+    // Because of lack of Python's ML libraries, will have to make functions for randomization of weights. CALL them here.
+
+
+    }
+
+
+     */
+
+
+
+    /**
+     * Feed forward function for Neural Network.
+     *
+     * Just puts values in, feeds them forward just like normal. Will call the activation function.
+     */
+    /*
+
+
+     */
+
+
+    /**
+     * Backpropagation function for Neural Network.
+     *
+     * Starting with targets and the outputted, instance-remembered raw values for each node, adjusts weights.
+     * Will call the activation function's derivative (input the raw values) and * the error.
+     *
+     * Then, will update weights. DOUBLE-CHECK this portion, it's the rockiest part concept-wise.
+     *
+     */
+    /*
+
+
+
+     */
+
+
+    /**
+     * 
+     */
+
+
+
+}
---
+++ src/java_MLP_NN/Tester_NN.java
@@ -0,0 +1,12 @@
+package java_MLP_NN;
+
+/**
+ * Created by Sarah on 9/25/2017.
+ */
+public class Tester_NN
+{
+
+    // May just have main method.
+
+
+}
